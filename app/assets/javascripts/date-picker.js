ce93c996f3e62f6482cc8c382a1e3ab53a6007bf
Mateusz Zając <zajac.mateusz@icloud.com>
2015-01-03 15:43:04 +0100

    Add custom datepicker js methods

---
+++ app/assets/javascripts/date-picker.js
@@ -0,0 +1,20 @@
+/**
+ * Created by cojoj on 03.01.15.
+ */
+
+$(document).on('ready page:change', function () {
+    $('#start_date_picker').datetimepicker({
+        direction: 'bottom',
+        pickTime: false
+    });
+    $('#end_date_picker').datetimepicker({
+        direction: 'bottom',
+        pickTime: false
+    });
+    $("#start_date_picker").on("dp.change", function (e) {
+        $('#end_date_picker').data("DateTimePicker").setMinDate(e.date.add(1, 'days'));
+    });
+    $("#end_date_picker").on("dp.change", function (e) {
+        $('#start_date_picker').data("DateTimePicker").setMaxDate(e.date);
+    });
+})
