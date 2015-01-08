e5301394f9a6a17903b78646c69e01ba9e0ae9f5
Mateusz Zając <zajac.mateusz@icloud.com>
2015-01-08 10:40:50 +0100

    Add sample autocomplete

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,3 +1,10 @@
 /**
  * Created by cojoj on 08.01.15.
  */
+$(document).on('ready page:change', function () {
+    var availableTags = ["ActionScript", "AppleScript", "Asp", "BASIC", "C", "C++", "Clojure", "COBOL", "ColdFusion", "Erlang", "Fortran", "Groovy", "Haskell", "Java", "JavaScript", "Lisp", "Perl", "PHP", "Python", "Ruby", "Scala", "Scheme"];
+
+    $("#tags").autocomplete({
+        source: availableTags
+    });
+})
