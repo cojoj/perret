f7b4aa1d7db2cc0ce53ce77624774d0aab242da4
Mateusz Zając <zajac.mateusz@icloud.com>
2015-01-10 16:05:04 +0100

    Add datepicker validation

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -4,9 +4,13 @@
 
 $(document).on('ready page:change', function () {
     $('#start_date_picker').datepicker({
-        dateFormat: 'yy-mm-dd'
+        dateFormat: 'yy-mm-dd',
+        onSelect: function (dateValue) {
+            $("#end_date_picker").datepicker("option", "minDate", dateValue);
+        }
     });
+
     $('#end_date_picker').datepicker({
         dateFormat: 'yy-mm-dd'
     });
-})
+});
