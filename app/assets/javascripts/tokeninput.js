eab7286d5eaa750e27d40dfebaed897da7652881
Mateusz Zając <zajac.mateusz@icloud.com>
2015-01-13 15:50:17 +0100

    Improvement in tokeninput

--- app/assets/javascripts/tokeninput.js
+++ app/assets/javascripts/tokeninput.js
@@ -1,20 +1,20 @@
 /**
  * Created by cojoj on 10.01.15.
  */
-$(document).on('page:change', function () {
+$(document).ready(function () {
     $.ajax({
         url: '/autocomplete.json',
         success: function(groups) {
             $('#tokeninput').tokenInput(groups, {
                 theme: "facebook",
                 tokenLimit: 10,
-                resultsLimit: 10,
-                searchingText: "",
+                resultsLimit: 3,
+                searchingText: "Wyszukuje",
                 noResultText: "Brak pasujących grup",
-                hintText: "",
+                hintText: "Wpisz nazwę grupy",
                 searchDelay: 0,
                 preventDuplicates: true
             })
         }
     })
-})
+});
