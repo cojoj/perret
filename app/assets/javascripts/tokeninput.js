0c84fc73f1fb16713f40bab8d356e4db1b8afd9b
Mateusz Zając <zajac.mateusz@icloud.com>
2015-01-13 22:17:13 +0100

    Fix tokeninput

--- app/assets/javascripts/tokeninput.js
+++ app/assets/javascripts/tokeninput.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
             $('#tokeninput').tokenInput(groups, {
                 theme: "facebook",
                 tokenLimit: 10,
-                resultsLimit: 3,
+                resultsLimit: 10,
                 searchingText: "Wyszukuje",
                 noResultText: "Brak pasujących grup",
                 hintText: "Wpisz nazwę grupy",
